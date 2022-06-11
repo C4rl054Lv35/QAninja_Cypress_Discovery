05436fccdcbe9ea1351ca4b64737c8cc9934ad95
C4rl054Lv35 <karlosaugusto05@hotmail.com>
2022-06-11 00:31:16 -0300

    Correção Funcionando PASSED

--- buger-eats/cypress/integration/cadastro.spec.js
+++ buger-eats/cypress/integration/cadastro.spec.js
@@ -1,5 +1,9 @@
-describe('cadastro page', () => {
-            it('Usuário deve ser tornar um entregador', () => {
-                        cy.viewport(1440, 900)
-                        cy.visit('https://buger-eats.vercel.app')
-                        cy.get('a[href="/deliver"]').click()
+describe("cadastro page", () => {
+    it("Usuário deve ser tornar um entregador", () => {
+        cy.viewport(1440, 900);
+        cy.visit("https://buger-eats.vercel.app");
+        cy.get('a[href="/deliver"]').click();
+        cy.get("#page-deliver form h1")
+            .should("have.text", "Cadastre-se para  fazer entregas");
+    });
+});
