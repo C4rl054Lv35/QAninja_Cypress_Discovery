b07ab337302f86e8207185b9b074c68596fc97ec
C4rl054Lv35 <karlosaugusto05@hotmail.com>
2022-06-11 01:07:17 -0300

    Iron Man

--- buger-eats/cypress/integration/cadastro.spec.js
+++ buger-eats/cypress/integration/cadastro.spec.js
@@ -3,6 +3,6 @@ describe("cadastro page", () => {
         cy.viewport(1440, 900);
         cy.visit("https://buger-eats.vercel.app");
         cy.get('a[href="/deliver"]').click();
-        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para fazer entregas');
+        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas');
     });
 });
